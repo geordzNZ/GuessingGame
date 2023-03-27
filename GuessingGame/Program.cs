433dce25885bcf6582c9996e1fcfeb951b8e0f6f
GeordzNZ <geordie.king@gmail.com>
2023-03-27 23:18:38 +0100

    Get user input and convert to number

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -10,10 +10,18 @@
             Console.WriteLine("==========================================================\n");
 
             // Generate Random number
-            Random targetNumber = new Random();
-            int target = targetNumber.Next(1,100);
+            Random target = new Random();
+            int targetNumber = target.Next(1,100);
 
-            
+            // Game loop
+            bool targetGuessed = false;
+            while (!targetGuessed)
+            {
+                Console.Write($"\tGuess a number: ");
+                string guessedNumberInput = Console.ReadLine();
+                int guessedNumber = Int32.Parse(guessedNumberInput);
+
+            }
 
         }
     }
