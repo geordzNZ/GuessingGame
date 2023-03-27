2f311c7dde2db52448c528d6e79db81bce772837
GeordzNZ <geordie.king@gmail.com>
2023-03-28 00:31:25 +0100

    Implement the You're close functionality

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -2,6 +2,7 @@
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
             // Header section
@@ -15,7 +16,7 @@
 
             // Game loop
             bool targetGuessed = false;
-            int allowedWrongGuesses = 15;
+            int allowedWrongGuesses = 5;
             int guessCounter = 0;
             
             while (!targetGuessed && (guessCounter < allowedWrongGuesses))
@@ -27,28 +28,52 @@
                 if (guessedNumber == targetNumber)
                 {
                     guessCounter++;
-                    Console.WriteLine($"\n\n\tWINNER WINNER - YOU GUESSED { targetNumber } CORRECTLY IN { guessCounter } GUESSES!!!");
-
                     targetGuessed = true;
                 }
                 else
                 {
+                    bool closeGuess = false;
+                    if (Math.Abs(targetNumber - guessedNumber) <= 5)
+                    {
+                        closeGuess = true;
+                    }
+
                     if ((targetNumber - guessedNumber) >= 1)
                     {
-                        Console.Write($"\n\tToo low   --> ");
+                        Console.Write($"\n\tYour guess was TOO LOW ");
+                        if (closeGuess)
+                        {
+                            Console.Write($"but you are within 5 ");
+                        }
+                        Console.WriteLine("...");
                         guessCounter++;
                     }
                     else
                     {
-                        Console.Write($"\n\tToo high  --> ");
+                        Console.Write($"\n\tYour guess was TOO HIGH ");
+                        if (closeGuess)
+                        {
+                            Console.Write($"but you are within 5 ");
+                        }
+                        Console.WriteLine("...");
                         guessCounter++;
                     }
                 }
             }
 
-            Console.WriteLine($"\n\n\tTOO MANY GUESSES - YOU GUESSED { guessCounter } INCORRECT TIMES!!!");
+            if (targetGuessed)
+            {
+                Console.Write("\n\n\tWINNER WINNER!!!");
+                Console.Write($"\n\tYOU GUESSED {targetNumber} CORRECTLY");
+                Console.Write($"\n\tTAKING ONLY {guessCounter} GUESSES");
+            }
+            else
+            {
+                Console.Write("\n\n\tTOO MANY GUESSES!");
+                Console.Write($"\n\tYOU GUESSED {guessCounter} INCORRECT TIMES");
+                Console.Write($"\n\tTHE CORRECT NUMBER WAS {targetNumber}"); 
+            }
             Console.WriteLine("\n==========================================================\n");
-
         }
     }
 }
