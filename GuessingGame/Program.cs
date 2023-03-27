987fb9cdcd840aabab53ad9f7017f8f7f4371f5d
GeordzNZ <geordie.king@gmail.com>
2023-03-27 22:54:29 +0100

    Create Introduction text

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -4,6 +4,11 @@
     {
         static void Main(string[] args)
         {
+            // Header section
+            Console.WriteLine("\t\t\tWELCOME");
+            Console.WriteLine("\t\t  Number Guessing Game");
+            Console.WriteLine("==========================================================\n");
+
 
         }
     }
